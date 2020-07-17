45cb95936246e1b911244bf02f46d51a4ea28572
frsummit@simplexhub.com <www.frsummit1@gmail.com>
2020-07-18 04:21:34 +0600

    Update works successfully

--- app/src/main/java/com/frsummit/crud_spreadsheet/UpdateItem.java
+++ app/src/main/java/com/frsummit/crud_spreadsheet/UpdateItem.java
@@ -23,7 +23,6 @@ import com.android.volley.toolbox.Volley;
 public class UpdateItem extends Activity {
     ProgressDialog loading;
     EditText id, name, brand, price;
-    Button updateItem;
     String sheetDate, sheetId, itemId, itemName, itemBrand, itemPrice;
 
     @Override
@@ -48,42 +47,37 @@ public class UpdateItem extends Activity {
         name.setText(itemName);
         brand.setText(itemBrand);
         price.setText(itemPrice);
+    }
 
-        updateItem = findViewById(R.id.updateItem);
+    public void updateItemBtnClick(View view) {
+        loading = ProgressDialog.show(this, "Loading", "please wait", false, true);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, "https://script.google.com/macros/s/AKfycbw0lXmfX5kz3ZGM8P-G5hy4-sK-ewRbIHKUpaLj0IfWiROEhhtA/exec"
+                + "?action=updateItem"
+                + "&date=" + sheetDate
+                + "&id=" + sheetId
+                + "&itemId=" + id.getText()
+                + "&itemName=" + name.getText()
+                + "&brand=" + brand.getText()
+                + "&price=" + price.getText(),
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+                                loading.dismiss();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println("error : " + error);
+                    }
+                }
+        );
 
-        updateItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                loading = ProgressDialog.show(getApplicationContext(), "Loading", "please wait", false, true);
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, "https://script.google.com/macros/s/AKfycbw0lXmfX5kz3ZGM8P-G5hy4-sK-ewRbIHKUpaLj0IfWiROEhhtA/exec"
-                        + "?action=updateItem"
-                        + "&date=" + sheetDate
-                        + "&id=" + sheetId
-                        + "&itemId=" + id.getText()
-                        + "&itemName=" + name.getText()
-                        + "&brand=" + brand.getText()
-                        + "&price=" + price.getText(),
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
-//                                loading.dismiss();
-                            }
-                        },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                System.out.println("error : " + error);
-                            }
-                        }
-                );
-
-                int socketTimeOut = 50000;
-                RetryPolicy policy = new DefaultRetryPolicy(socketTimeOut, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
-                stringRequest.setRetryPolicy(policy);
-                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-                queue.add(stringRequest);
-            }
-        });
+        int socketTimeOut = 50000;
+        RetryPolicy policy = new DefaultRetryPolicy(socketTimeOut, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(policy);
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        queue.add(stringRequest);
     }
 }
