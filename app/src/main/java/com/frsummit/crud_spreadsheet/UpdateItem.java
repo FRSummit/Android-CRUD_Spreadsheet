7a27962c398796005d23767e91e66ac3cdf49759
frsummit@simplexhub.com <www.frsummit1@gmail.com>
2020-07-18 04:14:19 +0600

    Update append successfully

--- app/src/main/java/com/frsummit/crud_spreadsheet/UpdateItem.java
+++ app/src/main/java/com/frsummit/crud_spreadsheet/UpdateItem.java
@@ -3,13 +3,25 @@ package com.frsummit.crud_spreadsheet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class UpdateItem extends Activity {
+    ProgressDialog loading;
     EditText id, name, brand, price;
     Button updateItem;
     String sheetDate, sheetId, itemId, itemName, itemBrand, itemPrice;
@@ -42,7 +54,35 @@ public class UpdateItem extends Activity {
         updateItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println(id.getText());
+//                loading = ProgressDialog.show(getApplicationContext(), "Loading", "please wait", false, true);
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, "https://script.google.com/macros/s/AKfycbw0lXmfX5kz3ZGM8P-G5hy4-sK-ewRbIHKUpaLj0IfWiROEhhtA/exec"
+                        + "?action=updateItem"
+                        + "&date=" + sheetDate
+                        + "&id=" + sheetId
+                        + "&itemId=" + id.getText()
+                        + "&itemName=" + name.getText()
+                        + "&brand=" + brand.getText()
+                        + "&price=" + price.getText(),
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+//                                loading.dismiss();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                System.out.println("error : " + error);
+                            }
+                        }
+                );
+
+                int socketTimeOut = 50000;
+                RetryPolicy policy = new DefaultRetryPolicy(socketTimeOut, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+                stringRequest.setRetryPolicy(policy);
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                queue.add(stringRequest);
             }
         });
     }
